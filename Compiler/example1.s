192c63e6b444fe8ca9d33ecaf3ca8c2bb3c52287
sivakalki <sivakalkipusarla6@gmail.com>
2024-07-19 15:17:01 +0530

    added TinyC file

---
+++ Compiler/example1.s
@@ -0,0 +1,30 @@
+	.text			# The .text assembler directive indicates
+	.globl main		# The following is the code (as oppose to data)
+main:				# .globl makes main know to the outside of the program.
+#Prologue begins
+	sw $ra, 0($sp)		# Save the return address
+	sw $fp, -4($sp)		# Save the frame pointer
+	sub $fp, $sp, 8		# Save the frame pointer
+
+	sub $sp, $sp, 24		# Make space for the locals
+# Prologue ends
+
+	li $v0, 18
+	sw $v0, -12($fp)
+	li $v0, 48
+	sw $v0, -8($fp)
+	li $v0, 62
+	sw $v0, -4($fp)
+	li $v0, 1
+	lw $a0, 0($fp)
+	syscall
+	li $v0, 11
+	li $a0, 10
+	syscall
+# Epilogue Begins
+epilogue_main:
+	add $sp, $sp, 24
+	lw $fp, -4($sp)
+	lw $ra, 0($sp)
+	jr	$ra
+#Epilogue Ends
---
+++ Compiler/example1.s.s
@@ -0,0 +1,2 @@
+	.text			# The .text assembler directive indicates
+	.globl main		# The following is the code (as oppose to data)
